80ba7e1065ca738a76c092c0542c419e85697f33
Urzzard <manuel98_23@outlook.es>
2025-04-18 11:42:53 -0500

    change db properties

--- MANAGE/settings.py
+++ MANAGE/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'manage',
         'USER': 'urzzard',
-        'PASSWORD': 'bassflea23',
+        'PASSWORD': 'postgres',
         'HOST': 'localhost',
         'PORT': '5432',
     }
