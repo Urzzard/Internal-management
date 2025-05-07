1e1698d8842587254c21e4e33d49ebc54d6e4ce8
Urzzard <manuel98_23@outlook.es>
2025-05-06 22:52:01 -0500

    changing some aspects

--- client/src/features/Inicio.jsx
+++ client/src/features/Inicio.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react"
 import { useAuth } from "../context/AuthContext"
+import styles from './Inicio.module.css'
 export function Inicio(){
 
     const {user} = useAuth();
 
     return(
-        <div className="inicio">
+        <div className={styles.contenedor}>
             <h3>Bienvenido, {user.username} {user.is_superuser ? '(Admin)': user.is_staff ? '(Staff)': ''}</h3>
+            <div className={styles.stats}>
+                <div className={styles.statsPersonal}>
+                    
+                </div>
+            </div>
         </div>
     )
 }
